58d2eec0e2ea21ed50e3078049f97f309b878af3
Gary Hockin <gary@hock.in>
2016-12-05 08:47:05 +0000

    Day four part two, I'm not feeling good about todays solutions but they work at least

--- src/DayFour/Decoder.php
+++ src/DayFour/Decoder.php
@@ -12,4 +12,84 @@ namespace Advent2016\DayFour;
 class Decoder
 {
 
+    public function split(string $room, bool $removeDashes = true) : array
+    {
+        preg_match('#([a-z-]+\-)(\d+)\[([a-z]+)\]#', $room, $matches);
+        array_shift($matches);
+        if ($removeDashes) {
+            $matches[0] = str_replace('-', '', $matches[0]);
+        }
+        return $matches;
+    }
+
+    public function isValidRoom(string $name, string $checksum) : bool
+    {
+        $characterCount = $this->getCharacterCount($name);
+
+        $generatedChecksum = '';
+        foreach ($characterCount as $key => $value) {
+            $generatedChecksum .= $value['character'];
+            if (strlen($generatedChecksum) === 5) {
+                break;
+            }
+        }
+
+        return $generatedChecksum === $checksum;
+    }
+
+    public function getCharacterCount(string $roomName) : array
+    {
+        $count = [];
+        $key = 0;
+        $found = [];
+        foreach (str_split($roomName) as $char) {
+            if (!in_array($char, $found)) {
+                $found[] = $char;
+                $no = substr_count($roomName, $char);
+                $count[] = ['character' => $char, 'frequency' => $no];
+                $character[][$key] = $char;
+                $frequency[][$key] = $no;
+            }
+        }
+        array_multisort($frequency, SORT_DESC, $character, SORT_ASC, $count);
+        return $count;
+    }
+
+    public function decode(string $rooms, bool $partTwo = false) : int
+    {
+        $rooms = explode(PHP_EOL, $rooms);
+        $count = 0;
+        foreach ($rooms as $room) {
+            $split = $this->split($room);
+            list($name, $sectorId, $checksum) = $split;
+            if ($this->isValidRoom($name, $checksum)) {
+                $count += $sectorId;
+                if($partTwo && $this->decrypt($name, $sectorId) === 'northpoleobjectstorage') {
+                    return $sectorId;
+                }
+            }
+        }
+        return $count;
+    }
+
+    public function decrypt(string $roomName, int $sectorId) : string
+    {
+        $decrypted = '';
+        $roomNameCharacters = str_split($roomName);
+        foreach ($roomNameCharacters as $character) {
+            if($character === '-') {
+                $decrypted .= ' ';
+                continue;
+            }
+            $characterNo = ord($character);
+            for ($i = 0; $i < $sectorId; $i++) {
+                $characterNo++;
+                if($characterNo > ord('z')) {
+                    $characterNo = ord('a');
+                }
+            }
+            $decrypted .= chr($characterNo);
+        }
+        return trim($decrypted);
+    }
 }
--- test/Advent2016/DayFourTest/DecoderTest.php
+++ test/Advent2016/DayFourTest/DecoderTest.php
@@ -14,5 +14,84 @@ use Advent2016\DayFour\Decoder;
 
 class DecoderTest extends \PHPUnit_Framework_TestCase
 {
+    private $rooms = <<<ROOMS
+aaaaa-bbb-z-y-x-123[abxyz]
+a-b-c-d-e-f-g-h-987[abcde]
+not-a-real-room-404[oarel]
+totally-real-room-200[decoy]
+ROOMS;
+    /**
+     * @var Decoder
+     */
+    private $decoder;
 
+    public function setUp()
+    {
+        $this->decoder = new Decoder();
+    }
+
+    public function testExamplesPass()
+    {
+        self::assertEquals(1514, $this->decoder->decode($this->rooms));
+    }
+
+    public function testRoomSplitsCorrectly()
+    {
+        $room = 'aaaaa-bbb-z-y-x-123[abxyz]';
+        self::assertEquals([
+            'aaaaabbbzyx',
+            '123',
+            'abxyz'
+        ], $this->decoder->split($room));
+    }
+
+    public function testStringCharCount()
+    {
+        $room = 'aaaaabbbzxy';
+        $decodedRoom = [
+            ['character' => 'a', 'frequency' => 5],
+            ['character' => 'b', 'frequency' => 3],
+            ['character' => 'x', 'frequency' => 1],
+            ['character' => 'y', 'frequency' => 1],
+            ['character' => 'z', 'frequency' => 1],
+        ];
+        self::assertEquals($decodedRoom, $this->decoder->getCharacterCount($room));
+
+        $room = 'notarealroom';
+        $result = [
+            ['character' => 'o', 'frequency' => 3],
+            ['character' => 'a', 'frequency' => 2],
+            ['character' => 'r', 'frequency' => 2],
+            ['character' => 'e', 'frequency' => 1],
+            ['character' => 'l', 'frequency' => 1],
+            ['character' => 'm', 'frequency' => 1],
+            ['character' => 'n', 'frequency' => 1],
+            ['character' => 't', 'frequency' => 1],
+        ];
+
+        self::assertEquals($result, $this->decoder->getCharacterCount($room));
+    }
+
+    public function testRoomIsValid()
+    {
+        $room = 'aaaaa-bbb-z-y-x-123[abxyz]';
+        $decoded = $this->decoder->split($room);
+        self::assertTrue($this->decoder->isValidRoom($decoded[0], $decoded[2]));
+    }
+
+    public function testRoomIsInvalid()
+    {
+        $room = 'totally-real-room-200[decoy]';
+        $decoded = $this->decoder->split($room);
+        self::assertFalse($this->decoder->isValidRoom($decoded[0], $decoded[2]));
+    }
+
+    public function testCodeCanBeDecrypted()
+    {
+        $room = 'qzmt-zixmtkozy-ivhz-343[check]';
+        $decrypted = 'very encrypted name';
+
+        $decoded = $this->decoder->split($room, false);
+        self::assertEquals($decrypted, $this->decoder->decrypt($decoded[0], $decoded[1]));
+    }
 }
